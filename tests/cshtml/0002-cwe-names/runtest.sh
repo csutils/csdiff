afcd8f098987d4ed1499d12893e581ff6d1c3c33
Jan Macku <jamacku@redhat.com>
2021-12-06 14:21:07 +0100

    tests: Use json file extension for stored scan-results

--- tests/cshtml/0002-cwe-names/runtest.sh
+++ tests/cshtml/0002-cwe-names/runtest.sh
@@ -5,7 +5,7 @@ set -x
 # run cshtml
 "${CSHTML_BIN}" \
     --cwe-names "${TEST_SRC_DIR}/cwe-names.csv"         \
-    "${TEST_SRC_DIR}/scan-results.js"                   \
+    "${TEST_SRC_DIR}/scan-results.json"                   \
     > scan-results.html
 
 diff -up "${TEST_SRC_DIR}/scan-results.html" "${PWD}/scan-results.html"
--- tests/cslinker/0001-smoke/runtest.sh
+++ tests/cslinker/0001-smoke/runtest.sh
@@ -8,12 +8,12 @@ set -x
 # run cshtml
 "${CSLINKER_BIN}" \
     --cwelist "${TEST_SRC_DIR}/cwe-map.csv"             \
-    --implist "${TEST_SRC_DIR}/scan-results-imp.js"     \
+    --implist "${TEST_SRC_DIR}/scan-results-imp.json"   \
     --inifile "${TEST_SRC_DIR}/scan.ini"                \
     --reapply-parsing-rules                             \
     --quiet                                             \
     "${TEST_SRC_DIR}/uni-results"/*                     \
     | eval "${JSFILTER_CMD}"                            \
-    > scan-results.js
+    > scan-results.json
 
-diff -up "${TEST_SRC_DIR}/scan-results.js" "${PWD}/scan-results.js"
+diff -up "${TEST_SRC_DIR}/scan-results.json" "${PWD}/scan-results.json"
