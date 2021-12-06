993cb6686f0bd472f197f4a8eb758b3991a8fc50
Jan Macku <jamacku@redhat.com>
2021-12-06 14:21:07 +0100

    tests: Use json file extension for stored scan-results
    Closes: https://github.com/csutils/csdiff/pull/31

--- tests/cshtml/0001-smoke/runtest.sh
+++ tests/cshtml/0001-smoke/runtest.sh
@@ -5,10 +5,10 @@ set -x
 # run cshtml
 "${CSHTML_BIN}" \
     --cwe-names ""                                      \
-    --diff-base "${TEST_SRC_DIR}/old/scan-results.js"   \
+    --diff-base "${TEST_SRC_DIR}/old/scan-results.json" \
     --diff-base-ignore-checkers "SHELLCHECK_WARNING"    \
     --plain-text-url "scan-results.err"                 \
-    "${TEST_SRC_DIR}/scan-results.js"                   \
+    "${TEST_SRC_DIR}/scan-results.json"                 \
     > scan-results.html
 
 diff -up "${TEST_SRC_DIR}/scan-results.html" "${PWD}/scan-results.html"
--- tests/cslinker/0002-xml-parser/runtest.sh
+++ tests/cslinker/0002-xml-parser/runtest.sh
@@ -9,6 +9,6 @@ set -x
 "${CSLINKER_BIN}" --quiet                                       \
     "${TEST_SRC_DIR}/raw-results"/{pid-690565-2,xxx{2,}}.xml    \
     | eval "${JSFILTER_CMD}"                                    \
-    > scan-results.js
+    > scan-results.json
 
-diff -up "${TEST_SRC_DIR}/scan-results.js" "${PWD}/scan-results.js"
+diff -up "${TEST_SRC_DIR}/scan-results.json" "${PWD}/scan-results.json"
