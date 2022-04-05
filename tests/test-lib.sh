db90a8998018995a450ce20f9b165b30da4d9766
Kamil Dudka <kdudka@redhat.com>
2022-04-05 15:07:43 +0200

    tests: basic coverage for SARIF reading/writing
    Closes: https://github.com/csutils/csdiff/pull/39

--- tests/test-lib.sh
+++ tests/test-lib.sh
@@ -1,5 +1,8 @@
 # FIXME: copy-pasted from ../../CMakeLists.txt
 export JSFILTER_CMD="sed -e ':a;N;\$!ba;s|:\\n *\\[|: [|g'"
 
+# eliminate csdiff's version string in the output
+JSFILTER_CMD="${JSFILTER_CMD} -e 's|\"version\": \"[^\"]*\"|\"version\": \"\"|g'"
+
 # yet another conversion needed for scan properties on EPEL-7
 JSFILTER_CMD="${JSFILTER_CMD}';s|:\\n *{|: {|g'"
