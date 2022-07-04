45ffb742c9f029f300b137d8c166557397153064
Kamil Dudka <kdudka@redhat.com>
2022-07-04 14:18:44 +0200

    tests/cshtml/0002-cwe-names: use up2date cwe-names.csv
    So that the data file we maintain in this git repo is covered by a test.

--- tests/cshtml/0002-cwe-names/runtest.sh
+++ tests/cshtml/0002-cwe-names/runtest.sh
@@ -6,6 +6,6 @@ set -x
 "${CSHTML_BIN}" \
     --cwe-names "${TEST_SRC_DIR}/cwe-names.csv"         \
     "${TEST_SRC_DIR}/scan-results.json"                 \
-    > scan-results.html
+    > scan-results.html 2>&1
 
 diff -up "${TEST_SRC_DIR}/scan-results.html" "${PWD}/scan-results.html"
