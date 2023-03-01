30964cd493cca7103ecd79787aeddb599b1e2104
Lukáš Zaoral <lzaoral@redhat.com>
2023-03-01 10:40:05 +0100

    tests: remove redundant JSON output reformatters

--- tests/test-lib.sh
+++ tests/test-lib.sh
@@ -1,10 +1,5 @@
 # shellcheck shell=sh
 
 # FIXME: copy-pasted from ../../CMakeLists.txt
-export JSFILTER_CMD="sed -e ':a;N;\$!ba;s|:\\n *\\[|: [|g'"
-
 # eliminate csdiff's version string in the output
-JSFILTER_CMD="${JSFILTER_CMD} -e 's|\"version\": \"[^\"]*\"|\"version\": \"\"|g'"
-
-# yet another conversion needed for scan properties on EPEL-7
-JSFILTER_CMD="${JSFILTER_CMD}';s|:\\n *{|: {|g'"
+export JSFILTER_CMD="sed -e 's|\"version\": \"[^\"]*\"|\"version\": \"\"|g'"
