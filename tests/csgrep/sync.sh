b4a7f698bd87e356c7edaeb39d01ff6a9e079493
Lukáš Zaoral <lzaoral@redhat.com>
2023-03-06 16:16:17 +0100

    tests: fix csgrep/sync.sh with --filter-file tests
    Related: 653f312c2ce5f3150950a02e0ff40806a4027714
    Resolves: https://github.com/csutils/csdiff/issues/114

--- tests/csgrep/sync.sh
+++ tests/csgrep/sync.sh
@@ -1,6 +1,9 @@
 #!/bin/zsh
 set -exo pipefail
 
+# set path to project root
+PROJECT_ROOT="../../"
+
 # import ${JSFILTER_CMD}
 . ../test-lib.sh
 
@@ -12,7 +15,7 @@ fi
 
 for tst in "${tests[@]}"; do
     tst=${tst%-args.txt}
-    eval "../../csdiff_build/src/csgrep $(<${tst}-args.txt) ${tst}-stdin.txt" \
+    eval "$PROJECT_ROOT/csdiff_build/src/csgrep $(<${tst}-args.txt) ${tst}-stdin.txt" \
         | eval "${JSFILTER_CMD}" \
         > ${tst}-stdout.txt
 done
