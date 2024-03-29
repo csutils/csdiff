3765a4f888f14f62a23eb4d5accff479a30d85a2
Kamil Dudka <kdudka@redhat.com>
2024-03-29 12:52:43 +0100

    parser-json-gcc: read endLine/endColumn if available
    Otherwise fallback to `caret`, which was the only field used so far.
    
    Related: https://github.com/csutils/csdiff/issues/136

--- src/lib/parser-json-gcc.cc
+++ src/lib/parser-json-gcc.cc
@@ -21,6 +21,8 @@
 
 #include "parser-gcc.hh"            // for GccPostProcessor
 
+using std::string;
+
 struct GccTreeDecoder::Private {
     GccPostProcessor            postProc;
 };
@@ -32,10 +34,56 @@ GccTreeDecoder::GccTreeDecoder():
 
 GccTreeDecoder::~GccTreeDecoder() = default;
 
+static bool gccReadLocRegion(
+        DefEvent           *pEvt,
+        const pt::ptree    &start,
+        const pt::ptree    &finish)
+{
+    // read file name
+    pEvt->fileName = valueOf<string>(start, "file", "<unknown>");
+    if (pEvt->fileName != valueOf<string>(finish, "file", "<unknown>"))
+        return false;
+
+    // read line
+    if ((pEvt->line = valueOf<int>(start, "line"))) {
+        const int endLine = valueOf<int>(finish, "line");
+        pEvt->vSize = diffNums(pEvt->line, endLine);
+    }
+
+    // read column
+    if ((pEvt->column = valueOf<int>(start, "byte-column"))) {
+        const int endColumn = valueOf<int>(finish, "byte-column");
+        pEvt->hSize = diffNums(pEvt->column, endColumn);
+    }
+
+    return true;
+}
+
+static void gccReadLocation(DefEvent *pEvt, const pt::ptree *locs)
+{
+    if (locs->empty())
+        return;
+
+    const pt::ptree &firstLoc = locs->begin()->second;
+
+    // try to read a region between start..finish
+    const pt::ptree *start, *finish;
+    if (findChildOf(&start, firstLoc, "start")
+            && findChildOf(&finish, firstLoc, "finish")
+            && gccReadLocRegion(pEvt, *start, *finish))
+        return;
+
+    // fallback to caret
+    const pt::ptree *caret;
+    if (findChildOf(&caret, firstLoc, "caret")) {
+        pEvt->fileName  = valueOf<string>(*caret, "file", "<unknown>");
+        pEvt->line      = valueOf<int>   (*caret, "line");
+        pEvt->column    = valueOf<int>   (*caret, "byte-column");
+    }
+}
+
 static bool gccReadEvent(DefEvent *pEvt, const pt::ptree &evtNode)
 {
-    using std::string;
-
     // read kind (error, warning, note)
     string &evtName = pEvt->event;
     evtName = valueOf<string>(evtNode, "kind");
@@ -45,14 +93,8 @@ static bool gccReadEvent(DefEvent *pEvt, const pt::ptree &evtNode)
     // read location
     pEvt->fileName = "<unknown>";
     const pt::ptree *locs;
-    if (findChildOf(&locs, evtNode, "locations") && !locs->empty()) {
-        const pt::ptree *caret;
-        if (findChildOf(&caret, locs->begin()->second, "caret")) {
-            pEvt->fileName  = valueOf<string>(*caret, "file", "<unknown>");
-            pEvt->line      = valueOf<int>   (*caret, "line");
-            pEvt->column    = valueOf<int>   (*caret, "byte-column");
-        }
-    }
+    if (findChildOf(&locs, evtNode, "locations"))
+        gccReadLocation(pEvt, locs);
 
     // read message
     pEvt->msg = valueOf<string>(evtNode, "message", "<unknown>");
