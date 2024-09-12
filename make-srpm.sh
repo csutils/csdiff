csutils/csdiff

commit f16db5adb1c0ccb2619c0e3d11badbc606796ae1

packaging: do not use `%py_provides` for Python 2
The macro is not available in EPEL-7 buildroot and causes the build
of csdiff to fail:
```
error: line 79: Unknown tag: %py_provides    python2-csdiff
```

This partially reverts commit df5c63fbbc676220c0c13e3d3e4070beeab0489f.

Closes: https://github.com/csutils/csdiff/pull/202

make-srpm.sh
@@ -157,7 +157,7 @@ for context embedding in legacy build environments.
 %package -n python2-%{name}
 Summary:        Python interface to csdiff for Python 2
 BuildRequires:  python2-devel
-%py_provides    python2-%{name}
+%{?python_provide:%python_provide python2-%{name}}
 
 %description -n python2-%{name}
 This package contains the Python 2 binding for the csdiff tool for comparing

commit d1e25e81eeddde86a2b8d909d4256e3bd10fe848

packaging: do not use `%py_provides` in epel-7 builds
If the buildroot contains outdated packages that do not yet contain
definition of the macro, it causes the build of csdiff to fail with:
```
error: line 90: Unknown tag: %py_provides    python3-csdiff
```

This partially reverts commit df5c63fbbc676220c0c13e3d3e4070beeab0489f.

Fixes: commit f16db5adb1c0ccb2619c0e3d11badbc606796ae1
Closes: https://github.com/csutils/csdiff/pull/203

make-srpm.sh
@@ -171,7 +171,12 @@ code scan defect lists to find out added or fixed defects.
 %package -n python3-%{name}
 Summary:        Python interface to csdiff for Python 3
 BuildRequires:  python3-devel
+%if 0%{?rhel} == 7
+# fallback for epel7 buildroots with outdated RPM macros
+%{?python_provide:%python_provide python3-%{name}}
+%else
 %py_provides    python3-%{name}
+%endif
 
 %description -n python3-%{name}
 This package contains the Python 3 binding for the csdiff tool for comparing